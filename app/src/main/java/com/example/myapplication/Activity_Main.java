dc53bc19f1e38aa1035ca387cce55add54f3bfd5
rihila <rihilasumayya4092@gmail.com>
2024-06-25 00:55:43 +0600

    final commit

---
+++ app/src/main/java/com/example/myapplication/Activity_Main.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+    }
+
+    private void setContentView() {
+    }
+}
